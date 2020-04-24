a5f67207f97bd8fdb38db64b6597724b3ca5c444
BlasterWhite <38408069+BlasterWhite@users.noreply.github.com>
2020-04-24 17:19:11 +0200

    1.0.1

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 /*Base*/
+const botconfig = require("./botconfig.json");
 const { Client, MessageAttachment } = require('discord.js');
 const client = new Client();
 
-/*Variables*/
-const Prefix = '';
-const Token = 'NjU1ODM0ODMxODg3OTI1MjQ4.XqILaQ.Vs7ycqpzhYu2C9q4tsvcpFUymtM';
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity('Hello', {type: "WATCHING"});
 });
 
-client.on('message', msg => {
-  if (msg.content === Prefix + 'ping') {
-    msg.reply('Pong!');
-  }
-  if (msg.content === Prefix + '4k') {
-    const attachement = new MessageAttachment('https://i.imgur.com/CgRvwzj.jpeg')
-    msg.channel.send(attachement);
-  }
+client.on('message', async message => {
+    if(message.author.client)return;
+    if(message.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
 });
 
-client.login(Token);
+client.login(botconfig.token);
